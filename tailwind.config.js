c11ebb663f88b8a4630777ff8e99bd612e7582fe
Glovyhel <deafendttack37@gmail.com>
2022-12-26 22:58:14 +0700

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      zIndex : {
+        '1' : '1'
+      },
+    },
+  },
+  plugins: [],
+}
